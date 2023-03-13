55b3c012aa0d5b156f8d4eab8b79f87882967377
Bruna Santos <brunahta2025@hotmail.com>
2023-03-13 18:22:40 -0300

    page to app

---
+++ app/restaurant/[slug]/menu/page.tsx
@@ -0,0 +1,58 @@
+import { NavBar } from '../../../../components/navBar';
+import { Header } from '../../../../components/header';
+import s from './styles.module.scss';
+
+export default function RestaurantMenu(){
+    return(
+        <>
+    <main className={s.mainContainer}>
+        <main className={s.SecondMainContainer}>
+            {/* NAVBAR */}
+            <NavBar/>
+            {/* NAVBAR */} {/* HEADER */}
+            <header >
+            <div>
+                <h1 >
+                Milestones Grill (Toronto)
+                </h1>
+            </div>
+            </header>
+            {/* HEADER */} 
+            
+            {/* DESCRIPTION PORTION */}
+            <div className={s.description}>
+            <div className={s.div}>
+                {/* RESAURANT NAVBAR */}
+                <nav>
+                <a href=""> Overview </a>
+                <a href=""> Menu </a>
+                </nav>
+                {/* RESAURANT NAVBAR */} {/* MENU */}
+                <main>
+                <div>
+                    <div className={s.one}>
+                    <h1 >Menu</h1>
+                    </div>
+                    <div className={s.menuCard}>
+                    {/* MENU CARD */}
+                    <div>
+                        <h3>Surf and Turf</h3>
+                        <p>
+                        A well done steak with lobster and rice
+                        </p>
+                        <p className={s.p}>$80.00</p>
+                    </div>
+                    {/* MENU CARD */}
+                    </div>
+                </div>
+                </main>
+                {/* MENU */}
+            </div>
+            </div>
+            {/* DESCRIPTION PORTION */}
+        </main>
+        </main>
+
+        </>
+    )
+}
