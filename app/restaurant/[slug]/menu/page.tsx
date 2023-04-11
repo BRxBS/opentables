0343d8a4b861c8a56af886268ad89d1e551e68bc
Bruna Santos <brunahta2025@hotmail.com>
2023-04-11 17:59:23 -0300

    menu Refactoring

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,5 +1,7 @@
 import { NavBar } from '../../../../components/navBar';
-import { Header } from '../../../../components/header';
+import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import { RestaurantNavBar } from '../components/RestaurantNavBar';
 import s from './styles.module.scss';
 
 export default function RestaurantMenu(){
@@ -7,49 +9,18 @@ export default function RestaurantMenu(){
         <>
     <main className={s.mainContainer}>
         <main className={s.SecondMainContainer}>
-            {/* NAVBAR */}
+   
             <NavBar/>
-            {/* NAVBAR */} {/* HEADER */}
-            <header >
-            <div>
-                <h1 >
-                Milestones Grill (Toronto)
-                </h1>
-            </div>
-            </header>
-            {/* HEADER */} 
-            
-            {/* DESCRIPTION PORTION */}
+    
+            <Header/>
+          
             <div className={s.description}>
             <div className={s.div}>
-                {/* RESAURANT NAVBAR */}
-                <nav>
-                <a href=""> Overview </a>
-                <a href=""> Menu </a>
-                </nav>
-                {/* RESAURANT NAVBAR */} {/* MENU */}
-                <main>
-                <div>
-                    <div className={s.one}>
-                    <h1 >Menu</h1>
-                    </div>
-                    <div className={s.menuCard}>
-                    {/* MENU CARD */}
-                    <div>
-                        <h3>Surf and Turf</h3>
-                        <p>
-                        A well done steak with lobster and rice
-                        </p>
-                        <p className={s.p}>$80.00</p>
-                    </div>
-                    {/* MENU CARD */}
-                    </div>
-                </div>
-                </main>
-                {/* MENU */}
+
+            <RestaurantNavBar/> 
+            <Menu/>
             </div>
             </div>
-            {/* DESCRIPTION PORTION */}
         </main>
         </main>
 
