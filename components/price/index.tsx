1cc1f0d01c921d04003d2a760df822ff938fe2cf
Bruna Santos <brunahta2025@hotmail.com>
2023-04-20 17:29:56 -0300

    prisma data

--- components/price/index.tsx
+++ components/price/index.tsx
@@ -1,11 +1,35 @@
-import { RestaurantCardType } from '../../app/page';
+import { PRICE } from '@prisma/client';
 import styles from './styles.module.scss';
 
 
-export function Price(){
+export function Price({price}: {price:PRICE}){
+const renderPrice = () =>{
+    if (price === PRICE.CHEAP){
+        return(
+            <>
+            <span>$</span>
+            <span className={styles.priceText}>$$</span>
+            </>
+        )
+    } else if(price === PRICE.REGULAR){
+        return(
+            <>
+            <span>$$</span>
+            <span className={styles.priceText}>$</span>
+            </>
+        )
+    } else {
+        return(
+            <>
+            <span>$$$</span>
+            </>
+        )
+    }
+}
+
     return(
         <>
-
+        <p className={styles.Text}>{renderPrice()}</p>
         </>
     )
 }
